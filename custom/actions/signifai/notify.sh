c536dfa0b7d7d1b0e932ae4d9506f121587e6e25
Zach C <zach@signifai.io>
2017-07-21 14:55:16 -0700

    Now generating the JSON to submit to collector-s

--- custom/actions/signifai/notify.sh
+++ custom/actions/signifai/notify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# printf -v requires bash >= 3.1
+
 # Provide the parameter names in the order that they are provided. 
 # BASE_ARGS is the overarching argument list, but processing shifts
 # when NUMBER_OF_EVALUATION_ENTITIES is hit. At that point it starts
@@ -87,6 +89,125 @@ for arg in $BASE_ARGS; do
 done
 
 REGEX=$(echo "^(${BASE_ARGS// /|}|${EVALUATION_ENTITY_ARGS// /|}|${TRIGGERED_CONDITION_ARGS// /|}|${TC_BASELINE_ARGS// /|})" | sed -E 's:\|{2,}:|:g')
-declare | grep -E "$REGEX"
+#declare | grep -E "$REGEX"
 
+JSON_FILE=$(mktemp)
+if [ $? -ne 0 ]; then
+	echo "Couldn't safely create tempfile"
+	exit 1
+fi
 
+cat - > $JSON_FILE <<END_OF_CHUNK
+{
+    "version": 3,
+    "account": {
+        "id": "${ACCOUNT_ID}",
+        "name": "${ACCOUNT_NAME}"
+    },
+    "health_rule": {
+        "id": "${HEALTH_RULE_ID}",
+        "name": "${HEALTH_RULE_NAME}",
+        "duration": "${PVN_TIME_PERIOD_IN_MINUTES}"
+    },
+    "affected_entity": {
+        "type": "${AFFECTED_ENTITY_TYPE}",
+        "name": "${AFFECTED_ENTITY_NAME}",
+        "id": "${AFFECTED_ENTITY_ID}"
+    },
+    "application": {
+        "name": "${APP_NAME}",
+        "id": "${APP_ID}",
+    },
+    "alert": {
+        "time": "${PVN_ALERT_TIME}",
+        "tag": "${TAG}"
+    },
+    "summary_message": "${SUMMARY_MESSSAGE}",
+    "incident_id": "${INCIDENT_ID}",
+    "deep_link_url": "${DEEP_LINK_URL}",
+    "event_type": "${EVENT_TYPE}",
+    "priority": "${PRIORITY}",
+    "severity": "${SEVERITY}",
+    "evaluation_entities_number": "${NUMBER_OF_EVALUATION_ENTITIES}",
+    "evaluation_entities": [
+END_OF_CHUNK
+
+deref_ee() {
+	ACCESS_VAR="$1_$2"
+	echo "${!ACCESS_VAR}"
+}
+
+deref_tc() {
+	ACCESS_VAR="$1_$2_$3"
+	echo "${!ACCESS_VAR}"
+}
+
+for i in $(seq 1 ${NUMBER_OF_EVALUATION_ENTITIES}); do
+	ee_idx=$(($i-1))
+
+	cat - >> $JSON_FILE <<END_OF_CHUNK
+        {
+            "type": "$(deref_ee EVALUATION_ENTITY_TYPE $ee_idx)",
+            "name": "$(deref_ee EVALUATION_ENTITY_NAME $ee_idx)",
+            "id": "$(deref_ee EVALUATION_ENTITY_ID $ee_idx)",
+            "triggered_conditions_number": "$(deref_ee NUMBER_OF_TRIGGERED_CONDITIONS_PER_EVALUATION_ENTITY $ee_idx)",
+            "triggered_conditions": [
+END_OF_CHUNK
+    ee_tc_cnt_acc="NUMBER_OF_TRIGGERED_CONDITIONS_PER_EVALUATION_ENTITY_${ee_idx}"
+    for j in $(seq 1 ${!ee_tc_cnt_acc}); do
+    	tc_idx=$(($i-1))
+    	cat - >> $JSON_FILE <<END_OF_CHUNK
+                {
+                    "scope_type": "$(deref_tc SCOPE_TYPE $ee_idx $tc_idx)",
+                    "scope_name": "$(deref_tc SCOPE_NAME $ee_idx $tc_idx)",
+                    "scope_id": "$(deref_tc SCOPE_ID $ee_idx $tc_idx)",
+                    "condition_name": "$(deref_tc CONDITION_NAME $ee_idx $tc_idx)",
+                    "condition_id": "$(deref_tc CONDITION_ID $ee_idx $tc_idx)",
+                    "operator": "$(deref_tc OPERATOR $ee_idx $tc_idx)",
+                    "condition_unit_type": "$(deref_tc CONDITION_UNIT_TYPE $ee_idx $tc_idx)",
+                    "use_default_baseline": "$(deref_tc USE_DEFAULT_BASELINE $ee_idx $tc_idx)",
+END_OF_CHUNK
+		if [ "$(deref_tc USE_DEFAULT_BASELINE $ee_idx $tc_idx)" == "false" ]; then
+			cat - >> $JSON_FILE <<END_OF_CHUNK
+                    "baseline_name": "$(deref_tc BASELINE_NAME $ee_idx $tc_idx)",
+                    "baseline_id": "$(deref_tc BASELINE_ID $ee_idx $tc_idx)",
+END_OF_CHUNK
+		fi
+
+		cat - >> $JSON_FILE <<END_OF_CHUNK
+                    "threshold_value": "$(deref_tc THRESHOLD_VALUE $ee_idx $tc_idx)",
+                    "observed_value": "$(deref_tc OBSERVED_VALUE $ee_idx $tc_idx)"
+END_OF_CHUNK
+		if [ $j -lt ${!ee_tc_cnt_acc} ]; then
+			cat - >> $JSON_FILE <<END_OF_CHUNK
+                },
+END_OF_CHUNK
+		else
+			cat - >> $JSON_FILE <<END_OF_CHUNK
+                }
+END_OF_CHUNK
+		fi
+	done
+	cat - >> $JSON_FILE <<END_OF_CHUNK
+            ]
+END_OF_CHUNK
+	if [ $i -lt ${NUMBER_OF_EVALUATION_ENTITIES} ]; then
+		cat - >> $JSON_FILE <<END_OF_CHUNK
+        },
+END_OF_CHUNK
+	else
+		cat - >> $JSON_FILE <<END_OF_CHUNK
+        }
+END_OF_CHUNK
+	fi
+	cat - >> $JSON_FILE <<END_OF_CHUNK
+    ]
+END_OF_CHUNK
+done
+
+cat - >> $JSON_FILE <<END_OF_CHUNK
+}
+END_OF_CHUNK
+
+cat $JSON_FILE
+rm $JSON_FILE
